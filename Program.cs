aa1cb8e133b19331b4e39ed638c18f3dd678faca
MLundsgaard <36629202+MLundsgaard@users.noreply.github.com>
2018-03-13 06:23:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // The array, with values.
+            int[] data = { 1, 2, 3, 7, 7, 7, 7, 85, 152 };
+
+            // Method that sort the array.
+            Array.Sort(data);
+
+            // Method that loops throught the array for the sort method, and prints it out.
+            for (int i = 0; i < data.GetLength(0); i++)
+                Console.WriteLine(i + ": " + data[i]);
+
+            // Console Method that makes a new empty line.
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Total Sum of Array: " + SumMethod(data));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("The Average of the Array: " + AverageMethod(data));
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Since the loop became " + ReturnOnlySeven(data) + " " + "The method shows the numbers 7 that are in the array");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Returns true if the number 3 is in the Array: " + ReturnTrueIfThree(data));
+
+            Console.ReadLine();
+        }
+
+        // Method that adds the total sum of the array.
+        public static int SumMethod(int[] data)
+        {
+            return data.Sum();
+        }
+
+        // Method that returns the average of the elements in the array.
+        public static decimal AverageMethod(int[] data)
+        {
+            decimal result = (decimal)SumMethod(data) / data.Length;
+            return result;
+        }
+
+        // Method that loops through the Array, and write out the number 7 every time it becomes true.
+        public static bool ReturnOnlySeven(int[] data)
+        {
+            bool found = false;
+
+            foreach (int i in data)
+
+                if (i == 7)
+                {
+                    found = true;
+                    Console.WriteLine(i);
+                }
+
+            return found;
+        }
+
+        // Method that returns true if the Array contains the number 3.
+        public static bool ReturnTrueIfThree(int[] data)
+        {
+            bool found = false;
+
+            foreach (int i in data)
+                if (i == 3)
+                {
+                    found = true;
+                }
+
+            return found;
+        }
+    }
+}
